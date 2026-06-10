2c914c745bb7e9f3ba8c62a830fda9ca50e9a9a8
devrengaw <lucas.wagneer@gmail.com>
2026-06-10 11:51:37 -0300

    Update Login.tsx

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -91,13 +91,13 @@ export function Login() {
           <div className="flex w-full mt-8 p-1.5 bg-slate-100/80 rounded-2xl">
             <button 
               onClick={() => setActiveTab('login')}
-              className={`flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-bold transition-all ${activeTab === 'login' ? `bg-white text-${themeColor}-600 shadow-sm border border-slate-200/50` : 'text-slate-500 hover:text-slate-700'}`}
+              className={`flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-bold transition-all ${activeTab === 'login' ? 'bg-white text-' + themeColor + '-600 shadow-sm border border-slate-200/50' : 'text-slate-500 hover:text-slate-700'}`}
             >
               <LogIn size={18} /> Login
             </button>
             <button 
               onClick={() => setActiveTab('register')}
-              className={`flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-bold transition-all ${activeTab === 'register' ? `bg-white text-${themeColor}-600 shadow-sm border border-slate-200/50` : 'text-slate-500 hover:text-slate-700'}`}
+              className={`flex-1 flex items-center justify-center gap-2 py-2.5 rounded-xl text-sm font-bold transition-all ${activeTab === 'register' ? 'bg-white text-' + themeColor + '-600 shadow-sm border border-slate-200/50' : 'text-slate-500 hover:text-slate-700'}`}
             >
               <UserPlus size={18} /> Cadastrar
             </button>
